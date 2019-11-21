c7bc9f6783d5cceec0c827480bf5da96a770a00d
pupil <pupil@ivkhk.ee>
2019-11-21 11:10:11 +0200

    Сделали добавление книги

--- src/jptvr18library/App.java
+++ src/jptvr18library/App.java
@@ -5,17 +5,21 @@
  */
 package jptvr18library;
 
+import entity.Book;
 import java.util.Scanner;
+import myclasses.BookProvider;
 
 /**
  *
  * @author pupil
  */
 public class App {
+private Book book;
     public void run(){
         System.out.println("Консольная библиотека");
         
-        do{
+        OUTER:
+        do {
             System.out.println("0. Выход из программы");
             System.out.println("1. Новая книга");
             System.out.println("2. Список книг");
@@ -25,47 +29,32 @@ public class App {
             System.out.println("Выбериите задачу: ");
             Scanner scanner =  new Scanner(System.in);
             int task = scanner.nextInt();
-            if(0 == task){
-                System.out.println("Закрытие программы");
-                break;
+            switch (task) {
+                case 0:
+                    System.out.println("Закрытие программы");
+                    break OUTER;
+            //new book
+                case 1:
+                    BookProvider bookProvider = new BookProvider();
+                    book = bookProvider.createBook();
+                    break;
+            //list of books
+                case 2:
+                    break;
+            //register new Reader
+                case 3:
+                    break;
+            //give a book
+                case 4:
+                    break;
+            //return a book
+                case 5:
+                    break;
+                default:
+                    System.out.println("Такой задачи нет!");
+                    break;
             }
-            else if (1 == task){
-                //new book
-            }else if(2 == task){
-                //list of books
-
-            }else if(3 == task){
-                //register new Reader
-            }else if(4 == task){
-                //give a book
-            }else if(5 == task){
-                //return a book
-            }else{
-                System.out.println("Такой задачи нет!");
-            }
-        }while(true);
-        
-        
-//        Book book = new Book();
-//        book.setName("Travelling Java Coffee");
-//        book.setAuthor("JVM");
-//        book.setIsbn("542-4635");
-//        book.setPublishedyear(1991);
-//        System.out.println(book.toString());
-//        
-//        Reader reader = new Reader();
-//        reader.setName("Petja");
-//        reader.setPhone("3725350213");
-//        reader.setSurname("Ivanov");
-//        reader.setDay(5);
-//        reader.setMonth(1);
-//        reader.setYear(2001);
-//        System.out.println(reader.toString());
-//        
-//        History history = new History(new Date(),null,book,reader);
-//        System.out.println(history.toString());
-//        history.setReturnOfDate(new Date());
-//        System.out.println(history.toString());
+        } while (true);
         
     }
     
---
+++ src/myclasses/BookProvider.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclasses;
+
+import entity.Book;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class BookProvider {
+    public Book createBook(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("----Создание книги----");
+        System.out.println("Название книги: ");
+        String name = scanner.nextLine();
+        System.out.println("Автор книги:");
+        String author = scanner.nextLine();
+        System.out.println("Год издания:");
+        int publishedYear = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Isbn книги:");
+        String isbn = scanner.nextLine();
+        Book book = new Book(name, author, publishedYear, isbn);
+       
+        return book;
+    }
+}
